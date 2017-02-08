c1959a276919839074631eef317396bcda4f39a3
Manuel de Brito Fontes <aledbf@gmail.com>
2017-02-08 19:20:08 -0300

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # k8s-dump
 Dump object from a running Kubernetes cluster
+
+Options:
+```
+./dump --help
+      --alsologtostderr                  log to standard error as well as files
+      --apiserver-host string            The address of the Kubernetes Apiserver to connect to in the format of protocol://address:port, e.g., http://localhost:8080. If not specified, the assumption is that the binary runs inside a Kubernetes cluster and local discovery is attempted.
+      --kubeconfig string                Path to kubeconfig file with authorization and master location information.
+      --log_backtrace_at traceLocation   when logging hits line file:N, emit a stack trace (default :0)
+      --log_dir string                   If non-empty, write log files in this directory
+      --logtostderr                      log to standard error instead of files
+      --namespace string                 Only dump the contents of a particular namespace.
+      --output string                    Directory where the dump files should be created.
+      --skip-types stringSlice           Types to skip in the dump.  (default [serviceaccount])
+      --stderrthreshold severity         logs at or above this threshold go to stderr (default 2)
+      -v, --v Level                          log level for V logs
+      --vmodule moduleSpec               comma-separated list of pattern=N settings for file-filtered logging
+```
+
+In a terminal open a proxy connection to the cluster using kubectl:
+```
+kubectl proxy --port=8080
+Starting to serve on 127.0.0.1:8080
+```
+
+**Running the command:**
+```
+./dump \
+    --output=$PWD/out \
+    --apiserver-host=http://127.0.0.1:8080
+
+```
+
+will dump all the object types (excluding service accounts) in the directory `--output` creating one file per namespace.
+Each
+
+```
+./dump --output=/Users/aledbf/go/src/k8s.io/dump/out --apiserver-host=http://127.0.0.1:8080
+I0208 21:00:09.493695   64091 main.go:116] Creating API server client for http://127.0.0.1:8080
+I0208 21:00:09.883821   64091 main.go:146] Dumping cluster objects...
+I0208 21:00:09.883845   64091 main.go:218] 	dumping namespace xxxxxxx
+I0208 21:00:13.830584   64091 main.go:218] 	dumping namespace xxxxxxx
+...
+I0208 21:00:19.830584   64091 main.go:158] 	done.
+```
+
+Example of a file:
+````
+
+# errors:
+
+# there is no objects of type statefulsets in namespace xxxxxx
+# there is no objects of type storageclasses in namespace xxxxxx
+# there is no objects of type persistentvolumes in namespace xxxxxx
+# there is no objects of type podsecuritypolicies in namespace xxxxxx
+# there is no objects of type thirdpartyresources in namespace xxxxxx
+
+# namespace
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: xxxxxx
+
+---
+
+# deployments
+
+......
+---
+
+# endpoints
+
+......
+---
+
+# replicasets
+
+......
+---
+
+# secrets
+
+......
+---
+
+# services
+
+......
+---
+```
---
+++ main.go
@@ -0,0 +1,316 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	text_template "text/template"
+
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
+	"github.com/spf13/pflag"
+
+	"k8s.io/kubernetes/pkg/api"
+	k8s_errors "k8s.io/kubernetes/pkg/api/errors"
+	apps "k8s.io/kubernetes/pkg/apis/apps/v1beta1"
+	autoscalingapiv1 "k8s.io/kubernetes/pkg/apis/autoscaling/v1"
+	batch "k8s.io/kubernetes/pkg/apis/batch/v2alpha1"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	storage "k8s.io/kubernetes/pkg/apis/storage/v1beta1"
+	client "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	restclient "k8s.io/kubernetes/pkg/client/restclient"
+	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
+	clientcmdapi "k8s.io/kubernetes/pkg/client/unversioned/clientcmd/api"
+	"k8s.io/kubernetes/pkg/kubectl"
+	"k8s.io/kubernetes/pkg/runtime"
+)
+
+func main() {
+	var (
+		flags = pflag.NewFlagSet("", pflag.ExitOnError)
+
+		apiserverHost = flags.String("apiserver-host", "", "The address of the Kubernetes Apiserver "+
+			"to connect to in the format of protocol://address:port, e.g., "+
+			"http://localhost:8080. If not specified, the assumption is that the binary runs inside a "+
+			"Kubernetes cluster and local discovery is attempted.")
+		kubeConfigFile = flags.String("kubeconfig", "", "Path to kubeconfig file with authorization and master location information.")
+		skipTypes      = flags.StringSlice("skip-types", []string{"serviceaccount"}, "Types to skip in the dump. ")
+		output         = flags.String("output", "", "Directory where the dump files should be created.")
+		namespace      = flags.String("namespace", "", "Only dump the contents of a particular namespace.")
+	)
+
+	flags.AddGoFlagSet(flag.CommandLine)
+	flags.Parse(os.Args)
+
+	flag.Set("logtostderr", "true")
+
+	kubeClient, err := createApiserverClient(*apiserverHost, *kubeConfigFile)
+	if err != nil {
+		handleFatalInitError(err)
+	}
+
+	dumpCluster(kubeClient, *output, *namespace, *skipTypes)
+}
+
+const (
+	// High enough QPS to fit all expected use cases. QPS=0 is not set here, because
+	// client code is overriding it.
+	defaultQPS = 1e6
+	// High enough Burst to fit all expected use cases. Burst=0 is not set here, because
+	// client code is overriding it.
+	defaultBurst = 1e6
+
+	template = `
+# errors:
+{{ range $i, $v := .notFound }}
+# {{ $v }}{{ end }}
+
+# namespace
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: {{ .name }}
+
+---
+
+{{ template "iterate" . }}
+
+
+{{ define "iterate" }}
+{{ range $k, $v := .types }}
+{{- if ne (len $v.Items) 0 }}
+# {{ $k }}
+{{ range $item := $v.Items }}
+{{ objectToYaml $item }}
+
+---
+{{ end }}
+{{ end }}
+{{- end }}
+{{ end }}
+`
+)
+
+// createApiserverClient creates new Kubernetes Apiserver client. When kubeconfig or apiserverHost param is empty
+// the function assumes that it is running inside a Kubernetes cluster and attempts to
+// discover the Apiserver. Otherwise, it connects to the Apiserver specified.
+//
+// apiserverHost param is in the format of protocol://address:port/pathPrefix, e.g.http://localhost:8001.
+// kubeConfig location of kubeconfig file
+func createApiserverClient(apiserverHost string, kubeConfig string) (*client.Clientset, error) {
+
+	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfig},
+		&clientcmd.ConfigOverrides{ClusterInfo: clientcmdapi.Cluster{Server: apiserverHost}})
+
+	cfg, err := clientConfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.QPS = defaultQPS
+	cfg.Burst = defaultBurst
+	cfg.ContentType = "application/vnd.kubernetes.protobuf"
+
+	glog.Infof("Creating API server client for %s", cfg.Host)
+
+	client, err := client.NewForConfig(cfg)
+
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+/**
+ * Handles fatal init error that prevents server from doing any work. Prints verbose error
+ * message and quits the server.
+ */
+func handleFatalInitError(err error) {
+	glog.Fatalf("Error while initializing connection to Kubernetes apiserver. "+
+		"This most likely means that the cluster is misconfigured (e.g., it has "+
+		"invalid apiserver certificates or service accounts configuration). Reason: %s\n"+
+		"Refer to the troubleshooting guide for more information: "+
+		"https://github.com/kubernetes/ingress/blob/master/docs/troubleshooting.md", err)
+}
+
+// dump extracts information from a Kubernetes cluster and creates multiple
+// files (one per namespace) with the content
+func dumpCluster(kubeClient *client.Clientset, output, namespace string, skipTypes []string) {
+	nss, err := kubeClient.Namespaces().List(api.ListOptions{})
+	if err != nil {
+		glog.Fatalf("unexpected error obtaining information about the namespaces: %v", err)
+	}
+
+	glog.Infof("Dumping cluster objects...")
+	if namespace != "" {
+		dumpNamespace(kubeClient, namespace, output, skipTypes)
+		glog.Infof("done")
+		os.Exit(0)
+	}
+
+	for _, ns := range nss.Items {
+		if ns.Status.Phase == api.NamespaceTerminating {
+			glog.Infof("skiping namespace %v (is being terminated)", ns.Name)
+			continue
+		}
+
+		err := dumpNamespace(kubeClient, ns.Name, output, skipTypes)
+		if err != nil {
+			glog.Fatalf("unexpected error dumping namespace (%v) content: %v", ns.Name, err)
+		}
+	}
+	glog.Infof("done")
+}
+
+func newMappingFactoring() map[string]runtime.Object {
+	return map[string]runtime.Object{
+		"configmaps":               &api.ConfigMapList{},
+		"daemonsets":               &extensions.DaemonSetList{},
+		"deployments":              &extensions.DeploymentList{},
+		"endpoints":                &api.EndpointsList{},
+		"horizontalpodautoscalers": &autoscalingapiv1.HorizontalPodAutoscalerList{},
+		"ingresses":                &extensions.IngressList{},
+		"jobs":                     &batch.JobList{},
+		"limitranges":              &api.LimitRangeList{},
+		"networkpolicies":          &extensions.NetworkPolicyList{},
+		"persistentvolumeclaims":   &api.PersistentVolumeClaimList{},
+		"persistentvolumes":        &api.PersistentVolumeList{},
+		"podsecuritypolicies":      &extensions.PodSecurityPolicyList{},
+		"podtemplates":             &api.PodTemplateList{},
+		"replicasets":              &extensions.ReplicaSetList{},
+		"replicationcontrollers":   &api.ReplicationControllerList{},
+		"resourcequotas":           &api.ResourceQuotaList{},
+		"secrets":                  &api.SecretList{},
+		"services":                 &api.ServiceList{},
+		"statefulsets":             &apps.StatefulSetList{},
+		"storageclasses":           &storage.StorageClassList{},
+		"thirdpartyresources":      &extensions.ThirdPartyResourceList{},
+	}
+}
+
+var (
+	funcMap = text_template.FuncMap{
+		"objectToYaml": objectToYaml,
+	}
+)
+
+// dumpNamespace extracts information about Kubernetes object.
+// The types are:
+//  configmaps
+//  daemonsets
+//  deployments
+//  endpoints
+//  horizontalpodautoscalers
+//  ingresses
+//  jobs
+//  limitranges
+//  networkpolicies
+//  persistentvolumeclaims
+//  persistentvolumes
+//  podsecuritypolicies
+//  podtemplates
+//  replicasets
+//  replicationcontrollers
+//  resourcequotas
+//  secrets
+//  services
+//  statefulsets
+//  storageclasses
+//  thirdpartyresources
+//
+//
+func dumpNamespace(kubeClient *client.Clientset, ns, output string, skipTypes []string) error {
+	glog.Infof("\tdumping namespace %v", ns)
+	content := make(map[string]interface{}, 22)
+	data := make(map[string]interface{}, 22)
+	notFound := []string{}
+
+	t, err := text_template.New("dump.tmpl").Funcs(funcMap).Parse(template)
+	if err != nil {
+		return errors.Wrap(err, "unexpected error parsing template")
+	}
+
+	for objectType, result := range newMappingFactoring() {
+		if skipType(objectType, skipTypes) {
+			continue
+		}
+
+		var rc restclient.Interface
+
+		switch objectType {
+		case "horizontalpodautoscalers":
+			rc = kubeClient.Autoscaling().RESTClient()
+		case "jobs":
+			rc = kubeClient.Batch().RESTClient()
+		case "statefulsets":
+			rc = kubeClient.Apps().RESTClient()
+		case "storageclasses":
+			rc = kubeClient.Storage().RESTClient()
+		case "daemonsets", "deployments", "ingresses", "networkpolicies", "podsecuritypolicies", "replicasets", "thirdpartyresources":
+			rc = kubeClient.Extensions().RESTClient()
+		default:
+			rc = kubeClient.Core().RESTClient()
+		}
+
+		err := rc.Get().
+			Namespace(ns).
+			Resource(objectType).
+			VersionedParams(&api.ListOptions{}, api.ParameterCodec).
+			Do().
+			Into(result)
+
+		if err != nil {
+			if !k8s_errors.IsNotFound(err) {
+				return errors.Wrap(err, "unexpected error queryng type")
+			}
+
+			notFound = append(notFound, fmt.Sprintf("there is no objects of type %v in namespace %v", objectType, ns))
+		}
+
+		data[objectType] = result
+	}
+
+	content["notFound"] = notFound
+	content["name"] = ns
+	content["types"] = data
+
+	tmplBuf := new(bytes.Buffer)
+	err = t.Execute(tmplBuf, content)
+	if err != nil {
+		return errors.Wrap(err, "unexpected error populating template")
+	}
+
+	path := fmt.Sprintf("%v/%v.yaml", output, ns)
+	return ioutil.WriteFile(path, tmplBuf.Bytes(), 0644)
+}
+
+func skipType(name string, typesToSkip []string) bool {
+	for _, st := range typesToSkip {
+		if name == st {
+			glog.Warningf("skipping type %v", name)
+			return true
+		}
+	}
+	return false
+}
+
+func objectToYaml(obj runtime.Object) string {
+	s, err := marshalYaml(obj)
+	if err != nil {
+		glog.Errorf("unexpected error converting object to yaml: %v", err)
+	}
+	return s
+}
+
+func marshalYaml(obj runtime.Object) (string, error) {
+	printer := &kubectl.YAMLPrinter{}
+	tmplBuf := new(bytes.Buffer)
+	err := printer.PrintObj(obj, tmplBuf)
+	if err != nil {
+		return "", err
+	}
+	return tmplBuf.String(), nil
+}
